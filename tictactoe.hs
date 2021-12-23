6bd741a944d073afd01d137cce82f2dd53bef92d
Megan Astraus <nicka@NICK-ASTRAUSs-MacBook-Pro.local>
2021-12-22 23:40:29 -0700

    edits

--- tictactoe.hs
+++ tictactoe.hs
@@ -1,9 +1,11 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 import System.IO
 import Data.List
 
 --The 'data' and 'instance' declarations were very new for me, so I implemented them from patterns I saw in other examples. After playing around with different iterations of Show and Eq (I tried doing deriving (Eq,Show) for a while), I decided on this.
 data Player = X | O
     deriving (Eq)
+
 data Cell = Occupied Player | Open Int
     deriving(Eq)
 
@@ -13,30 +15,28 @@ instance Show Player where
 
 instance Show Cell where
     show (Occupied a) = show a
-    show (Open a) = show a
+    show (Open a)     = show a
 
 --xs is a built-in mechanic for applying outputs to lists. renderRows, when invoked, will take the 3 corresponding values from the list 'board' (seen in the renderBoard function) and apply each sequentially (a/index 1 of the 3, b/index 2 of the 3,...)
 renderRows :: [Cell] -> String
-renderRows (a:b:c:xs) = (show a) ++ " | " ++ (show b) ++ " | " ++ (show c)
+renderRows (a:b:c:xs) = show a ++ " | " ++ show b ++ " | " ++ show c
 
 --Prints on the CLI the outcome of invoking renderRows in groups of 3, then applying the divider beneath each. Initially I had the divider in it's own function (columnDivider) but decided to print to the screen so renderBoard was not invoking as many functions. It would be interesting to see if this has any affect on performance. I also wanted to find a more 'elegant' method to create a grid, but a lot of the syntax and functions were very difficult to understand, and I prioritized the code being easy-to-read (mostly for my benefit when I come back to review it.)
 renderBoard :: [Cell] -> IO ()
 renderBoard board = do
     putStrLn $ renderRows top
-    putStrLn $ "----------"
+    putStrLn "----------"
     putStrLn $ renderRows middle
-    putStrLn $ "----------"
+    putStrLn "----------"
     putStrLn $ renderRows bottom
     where
-        top = take 3 board
+        top    = take 3 board
         middle = drop 3 . take 6 $ board
         bottom = drop 6 board
 
 --Since the board's index naturally runs 1 less than the smallest number (there is no 0 on the board), input-1 gets the index from the board and compares it to if that number equals an Open spot. placePiece can then be invoked, taking the index again, adding the player's symbol of x or o, and dropping the index after (which would be the number it was replacing.) I had the most trouble playing with inputs of 1 and 9 before realizing I would have to manually adjust the input to attribute for the index (0 wouldn't be an Open space on the board and 9 wasn't an index.)
 cellIsOpen :: [Cell] -> Int -> Bool
-cellIsOpen board input = if board !! (input-1) == (Open input) 
-    then True 
-    else False
+cellIsOpen board input = board !! (input-1) == Open input
 
 placePiece :: Int -> a -> [a] -> [a]
 placePiece input xo board = take input board ++ [xo] ++ drop (input+1) board
@@ -46,46 +46,45 @@ checkForWinner :: Player -> [Cell] -> Bool
 checkForWinner xo board = vertical xo board || horizonal xo board || diagonal xo board
 
 vertical :: Player -> [Cell] -> Bool
-vertical xo board = 
-    if 
-        board!!0 == (Occupied xo) && board!!3 == (Occupied xo) && board!!6 == (Occupied xo) || board!!1 == (Occupied xo) && board!!4 == (Occupied xo) && board!!7 == (Occupied xo) || board!!2 == (Occupied xo) && board!!5 == (Occupied xo) && board!!8 == (Occupied xo) 
-    then True 
-    else False
+vertical xo board =
+    head board == Occupied xo && board!!3 == Occupied xo && board!!6 == Occupied xo
+    || board!!1 == Occupied xo && board!!4 == Occupied xo && board!!7 == Occupied xo 
+    || board!!2 == Occupied xo && board!!5 == Occupied xo && board!!8 == Occupied xo
 
 horizonal :: Player -> [Cell] -> Bool
-horizonal xo board = 
-    if 
-        board!!0 == (Occupied xo) && board!!1 == (Occupied xo) && board!!2 == (Occupied xo) || board!!3 == (Occupied xo) && board!!4 == (Occupied xo) && board!!5 == (Occupied xo) || board!!6 == (Occupied xo) && board!!7 == (Occupied xo) && board!!8 == (Occupied xo) 
-    then True 
-    else False
+horizonal xo board =
+    head board == Occupied xo && board!!1 == Occupied xo && board!!2 == Occupied xo 
+    || board!!3 == Occupied xo && board!!4 == Occupied xo && board!!5 == Occupied xo 
+    || board!!6 == Occupied xo && board!!7 == Occupied xo && board!!8 == Occupied xo
 
 diagonal :: Player -> [Cell] -> Bool
-diagonal xo board = 
-    if 
-        board!!0 == (Occupied xo) && board!!4 == (Occupied xo) && board!!8 == (Occupied xo) || board!!2 == (Occupied xo) && board!!4 == (Occupied xo) && board!!6 == (Occupied xo) 
-    then True 
-    else False
+diagonal xo board =
+    head board == Occupied xo && board!!4 == Occupied xo && board!!8 == Occupied xo 
+    || board!!2 == Occupied xo && board!!4 == Occupied xo && board!!6 == Occupied xo
 
 changePlayers :: Player -> Player
 changePlayers X = O
 changePlayers O = X
 
 runGame :: Player -> [Cell] -> Int -> IO ()
-runGame player board guessCount = do
---guessCount increases +1 each time the game is ran inside the block (therefore increasing with each guess) because there are a maximum of 9 guesses. A total of 9 successful guesses automatically indicates a tied game when the board is filled and no winner is declared.
+runGame player board guessCount =
     if guessCount == 9
-        then putStrLn "It's a tie!"
+        then putStrLn "It's a tie! Type 'main' to play again."
     else do
         putStrLn $ " "
         renderBoard board
-        putStrLn $ (show player) ++ " 's turn, type the space you'd like to select:"
+        putStrLn $ show player ++ " 's turn, type the space you'd like to select:"
+        putStrLn $ " "
 --Changing the user's input into an Int that could be used in later functions ended up being a challenge. I attempted to set it as input <- getChar then change the Char to Int, but it did not work. Finally, I discovered this method in a different piece of Haskell code and decided to try it.
         input <- readLn::IO Int
 --Verifying that the input is valid (both the number not being higher than 9 and the space being open on the board) is obviously a priority in tic-tac-toe, and although this line correctly checks validity, the Exception throws an error before my 'else do' code block can run if the index is higher than 9. It looks like there might be a script that allows users to do just that (Control.Exception) but I have yet to test it due to wanting to keep the code as vanilla as possible.
         if cellIsOpen board input && input < 10 then do
             let newBoard = placePiece (input-1) (Occupied player) board
             if checkForWinner player newBoard then do
-                putStrLn $ (show player) ++ " is the Winner! Type 'main' to play again."
+                putStrLn $ " "
+                renderBoard newBoard
+                putStrLn $ " "
+                putStrLn $ show player ++ " is the Winner! Type 'main' to play again."
             else do
                 runGame (changePlayers player) newBoard (guessCount + 1)
 --This error code block runs if selected cell is not open or (supposedly) when the input is greater than 10.
